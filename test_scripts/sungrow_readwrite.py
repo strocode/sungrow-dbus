9e3ed8fb18a8da926ad5fdbba1d4d766f93cfd6a
Keith Bannister <keith.bannister@gmail.com>
2024-08-26 05:19:14 +0000

    More sungrow testing. its crap.

--- test_scripts/sungrow_readwrite.py
+++ test_scripts/sungrow_readwrite.py
@@ -18,4 +18,14 @@ for i in range(n):
     v = reg.registers[i]
     print(f'addr {addr}={v:d} 0x{v:x}')
 
+# set max power output = units of 0.1%
+max_output = 10
+addr = 5039 # export limitation value - units of 0.1 %
+#addr = 5039 # power limitation adjustment
+reg = c.write_register(addr-1,max_output,unit=1)
+reg = c.read_holding_registers(addr-1, 1, unit=1)
+print(f'set max output to {reg.registers[0]}')
+
+
+
 
